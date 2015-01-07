cfd18657091ff8da4f96411aef9493232408ae89
lizzy <lizzy.999@qq.com>
2015-01-07 22:56:14 +0800

    header footer

--- application/views/index.php
+++ application/views/index.php
@@ -9,8 +9,96 @@
 </head>
 
 <body>
-
-晏钧设计首页
-
+	<div class="main_header">
+    	<div class="main_header_left">
+        	<img src="<?php echo base_url('static/image/header1.png');?>" />
+        </div>
+        <div class="main_header_right">
+        	<div class="main_header_top">
+            	<img src="<?php echo base_url('static/image/header_title.png');?>" />
+                <p>Brand Tel:4001-606-909</p>
+            </div>
+            <div class="main_header_nav">
+            	<ul class="main_ul1">
+                	<li><a href="#">案例</a></li>
+                    <li><a href="#">服务</a></li>
+                    <li><a href="#">我们</a></li>
+                    <div class="cl"></div>
+                </ul>
+                <ul class="main_ul2">
+                	<li><a href="#">业务通道</a></li>
+                    <li><a href="#">加入我们</a></li>
+                    <li><a href="#">分享中心</a></li>
+                    <li><a href="#">新闻</a></li>
+                    <div class="cl"></div>
+                </ul>
+                <div class="cl"></div>
+            </div>
+        </div>
+        <div class="cl"></div>
+    </div>
+    
+    <div class="main_footer">
+    	<div class="main_footer_left">
+        	<div class="main_footer_tel">Brand Tel:&nbsp;4001-606-909</div>
+            <div class="main_footer_address">
+            	<div class="main_footer_address1">
+                	晏钧设计/河北石家庄
+                </div>
+                <div class="main_footer_address2">
+                	Add:河北省石家庄市广安大街16号美东国际C座2708</br>PC:050011&nbsp;&nbsp;&nbsp;&nbsp;Mail:777@yanjun.net
+                </div>
+                <div class="main_footer_address3">
+                	<input type="button" value="查看地图"/>
+                </div>
+                <div class="cl"></div>
+            </div>
+            <div class="main_footer_address">
+            	<div class="main_footer_address1">
+                	晏钧设计/北京
+                </div>
+                <div class="main_footer_address2">
+                	Add:北京市朝阳区东三环中39号建外SOHO2号楼0501</br>PC:100022
+                </div>
+                <div class="main_footer_address3">
+                	<input type="button" value="查看地图"/>
+                </div>
+                <div class="cl"></div>
+            </div>
+            <div class="main_footer_corp">
+            	<div class="main_footer_corp1">
+                	产业投资合作伙伴
+                </div>
+                <div class="main_footer_corp2">
+                	<ul>
+                	<li><a href="#">智行文化</a></li>
+                    <li><a href="#">海晏堂</a></li>
+                    <li><a href="#">尖峰影视</a></li>
+                    <li><a href="#">龙竹堂</a></li>
+                    <li><a href="#">道达咨询</a></li>
+                    <div class="cl"></div>
+                </ul>
+                </div>
+                <div class="cl"></div>
+            </div>
+        </div>
+        <div class="main_footer_right">
+        	<div class="main_footer_er">
+            	<img src="<?php echo base_url('static/image/footer2.png');?>" />
+                <p><晏钧设计微信公众号></p>
+            </div>
+            <div class="main_footer_er">
+            	<img src="<?php echo base_url('static/image/footer3.png');?>" />
+                <p style="padding-left:5px;"><晏钧设计电子名片></p>
+            </div>
+            <div class="cl"></div>
+            <div class="main_footer_copyright">
+                <div>©1994-2012 YANJUN DESIGN.ALL Rights Reserved </div>
+                <div style="padding-left:85px;">  冀ICP备0507081号</div>
+            </div>
+            <div class="cl"></div>
+        </div>
+        <div class="cl"></div>
+    </div>
 </body>
 </html>
