556582171e809a05ef42abdab1ee70d59438757c
lizzy <lizzy.999@qq.com>
2015-01-07 10:11:44 +0800

    配置autoload项目

--- application/views/index.php
+++ application/views/index.php
@@ -1,9 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"/>
 <head>
-	<meta charset="utf-8">
-	<title>晏钧设计</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="<?php echo base_url('static/css/home.css'); ?>"/>
+<script type="text/javascript" src="<?php echo base_url('static/js/jquery-1.8.0.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('static/js/pic.js'); ?>"></script>
+<title>晏钧设计</title>
 </head>
+
 <body>
 
 晏钧设计首页
