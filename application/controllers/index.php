bf3187ad4052bf484874587ca7a2b4e7342baf46
Mld <463471808@qq.com>
2015-01-09 16:33:18 +0800

    加入我们二级页面

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -21,10 +21,16 @@ class Index extends CI_Controller {
 		$this->load->view('news');
 		$this->load->view('common/common_footer');
 	}
-		public function joinus()
+	public function joinus()
 	{
 		$this->load->view('common/common_header');
 		$this->load->view('join_us');
+		$this->load->view('common/common_footer');
+	}
+	public function joinus2()
+	{
+		$this->load->view('common/common_header');
+		$this->load->view('join_us_second');
 		$this->load->view('common/common_footer');
 	}
 }
