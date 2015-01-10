26259228ecefe5b5f1f1907b70a3f50e04cd6fef
Mld <463471808@qq.com>
2015-01-10 11:22:47 +0800

    test

--- application/views/common/common_header.php
+++ application/views/common/common_header.php
@@ -3,6 +3,7 @@
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"/>
+<html xmlns:wb="http://open.weibo.com/wb"></html>
 <head>
 <meta property="wb:webmaster" content="b7a6eb002e61a8b5" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -11,7 +12,8 @@
 <link rel="stylesheet" href="<?php echo base_url('static/css/case.css'); ?>"/>
 <script type="text/javascript" src="<?php echo base_url('static/js/jquery-1.8.0.js'); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('static/js/news.js'); ?>"></script>
-
+<script src="http://tjs.sjs.sinajs.cn/open/api/js/wb.js" type="text/javascript" charset="utf-8"></script>
+<script src="http://connect.qq.com/widget/loader/loader.js" widget="shareqq" charset="utf-8"></script>
 <title>晏钧设计</title>
 </head>
 
--- application/views/news.php
+++ application/views/news.php
@@ -35,28 +35,33 @@
                 </div>
                 <a class="fade" href="http://www.baidu.com"></a>
                 <a class="share"><img class="pic1" src="<?php echo base_url('static/image/pic1.png');?>" width=22 height=22/></a>
-                <div class="qq_share">
-                <script type="text/javascript">
-					(function(){
-						var p = {
-						url:location.href, /*获取URL，可加上来自分享到QQ标识，方便统计*/
-						desc:'', /*分享理由(风格应模拟用户对话),支持多分享语随机展现（使用|分隔）*/
-						title:'<?php echo $title;?>', /*分享标题(可选)*/
-						summary:'hahahahah', /*分享摘要(可选)*/
-						pics:"http://picapi.ooopic.com/00/87/68/91b1OOOPIC4e.jpg", /*分享图片(可选)*/
-						flash: '', /*视频地址(可选)*/
-						site:'', /*分享来源(可选) 如：QQ分享*/
-						style:'102',
-						width:63,
-						height:24
-						};
-						var s = [];
-						for(var i in p){
-						s.push(i + '=' + encodeURIComponent(p[i]||''));
-						}
-						document.write(['<a class="qcShareQQDiv" href="http://connect.qq.com/widget/shareqq/index.html?',s.join('&'),'" target="_blank">分享到QQ</a>'].join(''));
-						})();
-				</script>
+                <div class="share">
+                    <a class="qq_share">
+                        <script type="text/javascript">
+                            (function(){
+                                var p = {
+                                url:location.href, /*获取URL，可加上来自分享到QQ标识，方便统计*/
+                                desc:'', /*分享理由(风格应模拟用户对话),支持多分享语随机展现（使用|分隔）*/
+                                title:'<?php echo $title;?>', /*分享标题(可选)*/
+                                summary:'hahahahah', /*分享摘要(可选)*/
+                                pics:"http://picapi.ooopic.com/00/87/68/91b1OOOPIC4e.jpg", /*分享图片(可选)*/
+                                flash: '', /*视频地址(可选)*/
+                                site:'', /*分享来源(可选) 如：QQ分享*/
+                                style:'101',
+                                width:96,
+                                height:24
+                                };
+                                var s = [];
+                                for(var i in p){
+                                s.push(i + '=' + encodeURIComponent(p[i]||''));
+                                }
+                                document.write(['<a class="qcShareQQDiv" href="http://connect.qq.com/widget/shareqq/index.html?',s.join('&'),'" 	target="_blank">分享到QQ</a>'].join(''));
+            })();
+                        </script>
+                    </a>
+                    <a class="weibo_share">
+                        <wb:publish button_type="gray"  default_text="#<?php echo $title;?>#" default_image="http://ww1.sinaimg.cn/square/3ec33750jw1e5fksj1fdvj2028028web.jpg"></wb:publish>
+                    </a>
                 </div>
                 <div class="cl"></div>
             </div>
