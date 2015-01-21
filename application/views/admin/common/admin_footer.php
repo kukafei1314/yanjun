c031baae45e1755bf0ecd24982f6400e770d52bd
Mld <463471808@qq.com>
2015-01-21 09:51:46 +0800

    引入js

--- application/views/admin/common/admin_footer.php
+++ application/views/admin/common/admin_footer.php
@@ -16,41 +16,7 @@
         var ue = UE.getEditor('ue_content');
     </script>
     
-    <!-- js placed at the end of the document so the pages load faster -->
-    <script src="<?php echo base_url('static/admin/assets/js/jquery.js');?>"></script>
-    <script src="<?php echo base_url('static/admin/assets/js/jquery-1.8.3.min.js');?>"></script>
-    <script src="<?php echo base_url('static/admin/assets/js/bootstrap.min.js');?>"></script>
-    <script class="include" type="text/javascript" src="<?php echo base_url('static/admin/assets/js/jquery.dcjqaccordion.2.7.js');?>"></script>
-    <script src="<?php echo base_url('static/admin/assets/js/jquery.scrollTo.min.js');?>"></script>
-    <script src="<?php echo base_url('static/admin/assets/js/jquery.nicescroll.js');?>" type="text/javascript"></script>
-    <script src="<?php echo base_url('static/admin/assets/js/jquery.sparkline.js');?>"></script>
-
-
-    <!--common script for all pages-->
-    <script src="<?php echo base_url('static/admin/assets/js/common-scripts.js');?>"></script>
-    
-    <script type="text/javascript" src="<?php echo base_url('static/admin/assets/js/gritter/js/jquery.gritter.js');?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('static/admin/assets/js/gritter-conf.js');?>"></script>
-
-    <!--script for this page
-    <script src="<?php //echo base_url('static/admin/assets/js/sparkline-chart.js');?>"></script>    
-	<script src="<?php //echo base_url('static/admin/assets/js/zabuto_calendar.js');?>"></script>	-->
-    
-    <!--script for this page-->
-	<script src="<?php echo base_url('static/admin/js/jquery-ui.js');?>"></script>    
-    <script src="<?php echo base_url('static/admin/assets/js/tasks.js');?>" type="text/javascript"></script> 
-    <script src="<?php echo base_url('static/admin/assets/js/bootstrap-inputmask/bootstrap-inputmask.min.js');?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('static/admin/assets/js/bootstrap-datepicker/js/bootstrap-datepicker.js');?>"></script>
-	<script type="text/javascript" src="<?php echo base_url('static/admin/assets/js/bootstrap-daterangepicker/daterangepicker.js');?>"></script>
-    <!--script for this page-->
-    <script src="<?php echo base_url('static/admin/assets/js/jquery-ui-1.9.2.custom.min.js');?>"></script>
-
-	<!--custom switch-->
-	<script src="<?php echo base_url('static/admin/assets/js/bootstrap-switch.js');?>"></script>
-	
-	<!--custom tagsinput-->
-	<script src="<?php echo base_url('static/admin/assets/js/jquery.tagsinput.js');?>"></script>
-    <script src="<?php echo base_url('static/admin/assets/js/form-component.js');?>"></script> 
+   
    	
   </body>
 </html>
