c9cdb623e0a52d9bdb6c66b873f442b58ca67043
Mld <463471808@qq.com>
2015-03-24 21:34:00 +0800

    分享

--- application/views/about_us.php
+++ application/views/about_us.php
@@ -36,32 +36,18 @@
                     <div class="share">
                         <img class="pic1" src="<?php echo base_url('static/image/pic1.png');?>" width=22 height=22/>
                         <div class="share_1">
-                            <div class="qq_share">
-                                <script type="text/javascript">
-                                    (function(){
-                                        var p = {
-                                        url:location.href, /*获取URL，可加上来自分享到QQ标识，方便统计*/
-                                        desc:'', /*分享理由(风格应模拟用户对话),支持多分享语随机展现（使用|分隔）*/
-                                        title:"<?php echo $new['title'];?>", /*分享标题(可选)*/
-                                        summary:"<?php echo mb_substr( strip_tags($new['content']),0,100,'utf-8');?>", /*分享摘要(可选)*/
-                                        pics:"<?php echo base_url($new['images']);?>", /*分享图片(可选)*/
-                                        site:'', /*分享来源(可选) 如：QQ分享*/
-                                        style:'102',
-                                        width:63,
-                                        height:24
-                                        };
-                                        var s = [];
-                                        for(var i in p){
-                                        s.push(i + '=' + encodeURIComponent(p[i]||''));
-                                        }
-                                        document.write(['<a class="qcShareQQDiv" href="http://connect.qq.com/widget/shareqq/index.html?',s.join('&'),'" 	target="_blank">分享到QQ</a>'].join(''));
-                    })();
-                                </script>
-                            </div>
-                            <div class="weibo_share">
-                                <wb:share-button appkey="4BD8Tl" addition="simple" type="button" size="big" title="<?php echo $new['title'];?>" pic="<?php echo base_url($new['images']);?>"></wb:share-button>
-                            </div>
-                            <div class="cl"></div>
+                            <div id="bdshare" class="bdshare_t bds_tools get-codes-bdshare" data="{'text':'<?php echo $new['title'];?>','url':'http://yj.te168.cn/news','pic':'<?php echo base_url($new['images']);?>'}">
+                            <a class="bds_qzone">QQ </a>
+                            <a class="bds_tsina">新浪</a>
+                            <a class="bds_tqq">腾讯</a>
+                            <a class="bds_renren">人人</a>
+                            <span class="bds_more">更多</span>
+                        </div>
+                        <script type="text/javascript" id="bdshare_js" data="type=tools" ></script>
+                            <script type="text/javascript" id="bdshell_js"></script>
+                            <script type="text/javascript">
+                                document.getElementById("bdshell_js").src = "http://bdimg.share.baidu.com/static/js/shell_v2.js?cdnversion=" + Math.ceil(new Date()/3600000);
+                            </script>
                         </div>
                     </div> 
                 </div>
--- application/views/index.php
+++ application/views/index.php
@@ -56,33 +56,20 @@
                 <p>Time /  <?php echo $case['date']; ?> </p>
                 <div class="share_work">
                 <img style="margin-left:30px" src="<?php echo base_url('static/image/pic1.png'); ?>"/>
-                <div class="share_2">
-                    <div class="qq_share">
-                        <script type="text/javascript">
-                            (function(){
-                                var p = {
-                                url:location.href, /*获取URL，可加上来自分享到QQ标识，方便统计*/
-                                desc:'', /*分享理由(风格应模拟用户对话),支持多分享语随机展现（使用|分隔）*/
-                                title:'<?php echo $case['name'];?>', /*分享标题(可选)*/
-                                summary:'', /*分享摘要(可选)*/
-                                pics:"<?php echo base_url($case['images']); ?>", /*分享图片(可选)*/
-                                flash: '', /*视频地址(可选)*/
-                                site:'', /*分享来源(可选) 如：QQ分享*/
-                                style:'102',
-                                width:63,
-                                height:24
-                                };
-                                var s = [];
-                                for(var i in p){
-                                s.push(i + '=' + encodeURIComponent(p[i]||''));
-                                }
-                                document.write(['<a class="qcShareQQDiv" href="http://connect.qq.com/widget/shareqq/index.html?',s.join('&'),'" 	target="_blank">分享到QQ</a>'].join(''));
-            })();
-                            </script>
-						</div>
-						<div class="weibo_share">
-							<wb:share-button appkey="4BD8Tl" addition="simple" type="button" size="big" title="<?php echo $case['name'];?>" pic="<?php echo base_url($case['images']); ?>" width="63" height="26"></wb:share-button>
-						</div>
+                	<div class="share_2">
+                   		 <div id="bdshare" class="bdshare_t bds_tools get-codes-bdshare" data="{'text':'<?php echo $case['name'];?>','url':'http://yj.te168.cn/cases_expand?id=<?php echo $case['id'];?>','pic':'<?php echo base_url($case['images']);?>'}">
+                            <a class="bds_qzone">QQ </a>
+                            <a class="bds_tsina">新浪</a>
+                            <a class="bds_tqq">腾讯</a>
+                            <a class="bds_renren">人人</a>
+                            <span class="bds_more">更多</span>
+                        </div>
+                        <script type="text/javascript" id="bdshare_js" data="type=tools" ></script>
+                            <script type="text/javascript" id="bdshell_js"></script>
+                            <script type="text/javascript">
+                                document.getElementById("bdshell_js").src = "http://bdimg.share.baidu.com/static/js/shell_v2.js?cdnversion=" + Math.ceil(new Date()/3600000);
+                        </script>
+						
 					</div>
 				</div>
                 
@@ -95,7 +82,8 @@
     	<div class="main_news_title">
         	News
         </div>
-        <?php foreach($news as $new): ?>
+        <?php foreach($news as $new): 
+		$id = $new['id'];?>
     	 <div class="news_list">
                     <div class="news_list_word2">
                         <?php echo $new['title'];?>
@@ -107,32 +95,18 @@
                     <div class="share">
                         <img class="pic1" src="<?php echo base_url('static/image/pic1.png');?>" width=22 height=22/>
                         <div class="share_1">
-                            <div class="qq_share">
-                                <script type="text/javascript">
-                                    (function(){
-                                        var p = {
-                                        url:location.href, /*获取URL，可加上来自分享到QQ标识，方便统计*/
-                                        desc:'', /*分享理由(风格应模拟用户对话),支持多分享语随机展现（使用|分隔）*/
-                                        title:"<?php echo $new['title'];?>", /*分享标题(可选)*/
-                                        summary:"<?php echo mb_substr( strip_tags($new['content']),0,100,'utf-8');?>", /*分享摘要(可选)*/
-                                        pics:"<?php echo base_url($new['images']);?>", /*分享图片(可选)*/
-                                        site:'', /*分享来源(可选) 如：QQ分享*/
-                                        style:'102',
-                                        width:63,
-                                        height:24
-                                        };
-                                        var s = [];
-                                        for(var i in p){
-                                        s.push(i + '=' + encodeURIComponent(p[i]||''));
-                                        }
-                                        document.write(['<a class="qcShareQQDiv" href="http://connect.qq.com/widget/shareqq/index.html?',s.join('&'),'" 	target="_blank">分享到QQ</a>'].join(''));
-                    })();
-                                </script>
-                            </div>
-                            <div class="weibo_share">
-                                <wb:share-button appkey="4BD8Tl" addition="simple" type="button" size="big" title="<?php echo $new['title'];?>" pic="<?php echo base_url($new['images']);?>"></wb:share-button>
-                            </div>
-                            <div class="cl"></div>
+                            <div id="bdshare" class="bdshare_t bds_tools get-codes-bdshare" data="{'text':'<?php echo $new['title'];?>','url':'http://yj.te168.cn/news','pic':'<?php echo base_url($new['images']);?>'}">
+                            <a class="bds_qzone">QQ </a>
+                            <a class="bds_tsina">新浪</a>
+                            <a class="bds_tqq">腾讯</a>
+                            <a class="bds_renren">人人</a>
+                            <span class="bds_more">更多</span>
+                        </div>
+                        <script type="text/javascript" id="bdshare_js" data="type=tools" ></script>
+                            <script type="text/javascript" id="bdshell_js"></script>
+                            <script type="text/javascript">
+                                document.getElementById("bdshell_js").src = "http://bdimg.share.baidu.com/static/js/shell_v2.js?cdnversion=" + Math.ceil(new Date()/3600000);
+                            </script>
                         </div>
                     </div> 
                 </div>
