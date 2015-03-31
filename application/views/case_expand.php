6bd0a83656b37b94fd1e8679fe08128d55592512
kukafei <652316096@qq.com>
2015-03-31 20:17:29 +0800

    aa

--- application/views/case_expand.php
+++ application/views/case_expand.php
@@ -46,7 +46,7 @@
         <div class="case_right float">
 			<?php foreach( $cases as $item): ?>
 			<!--<a href="<?php// echo base_url('cases_expand?id='.$item['id']);?>"> -->
-			<a class="case_left_expand" onclick="get_single_cases(<?php echo $item['id'];?>)">
+			<!--<a class="case_left_expand" onclick="get_single_cases(<?php echo $item['id'];?>)">-->
 			
 			<div class="div_opa">
                 <img src="<?php echo $item['logo'];?>" width='100%' height='100%'/><br/>
@@ -54,33 +54,18 @@
                 <div class="share share1" style="position:absolute; left:165px">
                     <img class="pic1" src="<?php echo base_url('static/image/pic1.png');?>" width='22' height='22'/>
                     <div class="share_1">
-                        <div class="qq_share">
+                        <div id="bdshare" class="bdshare_t bds_tools get-codes-bdshare" data="{'text':'<?php echo $item['name'];?>','url':'http://yj.te168.cn/cases','pic':'<?php echo base_url($item['logo']);?>'}">
+                            <a class="bds_qzone">QQ </a>
+                            <a class="bds_tsina">新浪</a>
+                            <a class="bds_tqq">腾讯</a>
+                            <a class="bds_renren">人人</a>
+                            <span class="bds_more">更多</span>
+                        </div>
+                        <script type="text/javascript" id="bdshare_js" data="type=tools" ></script>
+						<script type="text/javascript" id="bdshell_js"></script>
                         <script type="text/javascript">
-                                (function(){
-                                    var p = {
-                                    url:location.href, /*获取URL，可加上来自分享到QQ标识，方便统计*/
-                                    desc:'', /*分享理由(风格应模拟用户对话),支持多分享语随机展现（使用|分隔）*/
-                                    title:'<?php echo $item['name'];?>', /*分享标题(可选)*/
-                                    summary:'hahahahah', /*分享摘要(可选)*/
-                                    pics:"http://picapi.ooopic.com/00/87/68/91b1OOOPIC4e.jpg", /*分享图片(可选)*/
-                                    flash: '', /*视频地址(可选)*/
-                                    site:'', /*分享来源(可选) 如：QQ分享*/
-                                    style:'102',
-                                    width:63,
-                                    height:24
-                                    };
-                                    var s = [];
-                                    for(var i in p){
-                                    s.push(i + '=' + encodeURIComponent(p[i]||''));
-									//s.push(i + '=' + p[i]||'');
-                                    }
-                                    document.write(['<a class="qcShareQQDiv" href="http://connect.qq.com/widget/shareqq/index.html?',s.join('&'),'" 	target="_blank">分享到QQ</a>'].join(''));
-                })();
-                            </script>
-                        </div>
-                        <div class="weibo_share">
-                            <wb:share-button appkey="4BD8Tl" addition="simple" type="button" size="big" width="63" height="26" pic="http://picapi.ooopic.com/00/87/68/91b1OOOPIC4e.jpg" title="<?php echo $item['name'];?>"></wb:share-button>
-                        </div>
+                            document.getElementById("bdshell_js").src = "http://bdimg.share.baidu.com/static/js/shell_v2.js?cdnversion=" + Math.ceil(new Date()/3600000);
+                        </script>
                     </div>
                 </div>
                
