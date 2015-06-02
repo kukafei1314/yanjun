672aea04b606048028066137e9dc071ff06bf20b
lizzy <lizzy.999@qq.com>
2015-06-02 15:59:41 +0800

    常见问题详情页面

--- application/views/subBusiness.php
+++ application/views/subBusiness.php
@@ -68,7 +68,7 @@
         	<p>常见问题</p>
             <div class="text_li">
 			    <?php foreach ($pro as $pro): ?>
-					<a href="servicechannel/businesspro?&p=<?php if ($pro['id']%5!=0){
+					<a href="servicechannel/businesspro?p=<?php if ($pro['id']%5!=0){
 																	$a=($pro['id']-$pro['id']%5+5)/5;
 																}else{
 																	$a=$pro['id']/5;
